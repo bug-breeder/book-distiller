9ef8c0454f75cc90df99f1743c1442b415de3a37
Alan Nguyen <anhngw@gmail.com>
2026-04-03 17:19:37 +0700

    feat: add shared parser types

---
+++ src/parser/types.ts
@@ -0,0 +1,37 @@
+export interface Chapter {
+  chapterNumber: number;
+  chapterTitle: string;
+  content: string;
+  wordCount: number;
+  pageRange?: { start: number; end: number };
+}
+
+export interface RawBookInfo {
+  title: string;
+  author: string;
+  language: string;
+  sourceFile: string;
+}
+
+export interface ParseResult {
+  info: RawBookInfo;
+  chapters: Chapter[];
+}
+
+export interface ChapterIndex {
+  chapterNumber: number;
+  chapterTitle: string;
+  wordCount: number;
+  file: string;
+}
+
+export interface BookMetadata {
+  slug: string;
+  title: string;
+  author: string;
+  language: string;
+  sourceFile: string;
+  parsedAt: string;
+  chapterCount: number;
+  chapters: ChapterIndex[];
+}
